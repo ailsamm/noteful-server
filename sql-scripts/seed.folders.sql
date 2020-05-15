8046f27920b2b042bec66150cda5108adc59aade
Ailsa Meechan-Maddon <ailsamm@outlook.com>
2020-05-15 09:34:48 -0700

    Create seed scripts

---
+++ sql-scripts/seed.folders.sql
@@ -0,0 +1,7 @@
+INSERT INTO noteful_folders (folder_name)
+VALUES
+  ('Folder name 1'),
+  ('Folder name 2'),
+  ('Folder name 3'),
+  ('Folder name 4'),
+  ('Folder name 5');
---
+++ sql-scripts/seed.notes.sql
@@ -0,0 +1,18 @@
+INSERT INTO noteful_notes (note_name, content, folder_id)
+VALUES
+  ('Note 1', 'Here is my content...', 1),
+  ('Note 2', 'Here is my content...', 1),
+  ('Note 3', 'Here is my content...', 1),
+  ('Note 4', 'Here is my content...', 1),
+  ('Note 5', 'Here is my content...', 2),
+  ('Note 6', 'Here is my content...', 2),
+  ('Note 7', 'Here is my content...', 2),
+  ('Note 8', 'Here is my content...', 2),
+  ('Note 9', 'Here is my content...', 3),
+  ('Note 10', 'Here is my content...', 3),
+  ('Note 11', 'Here is my content...', 3),
+  ('Note 12', 'Here is my content...', 3),
+  ('Note 13', 'Here is my content...', 4),
+  ('Note 14', 'Here is my content...', 4),
+  ('Note 15', 'Here is my content...', 5),
+  ('Note 16', 'Here is my content...', 5);
